3851224c5761ceb7c3dabb4ac93cd048dada40a0
Jakub Czesak <jakub.czesak@cbre.com>
2023-06-27 00:58:07 +0200

    Fix table managment features

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
 import { Container } from "react-bootstrap";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useParams, Navigate } from 'react-router-dom';
 import Home from "./components/pages/Home";
 import Table from "./components/pages/Table";
 import NotFound from "./components/pages/NotFound";
 import Header from "./components/views/Header";
 import Footer from "./components/views/Footer";
 import { fetchTables } from "./Redux/tablesRedux";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect} from "react";
 
 const App = () => {
   const dispatch = useDispatch();
 
-  useEffect(() => dispatch(fetchTables()), [dispatch]);
+  useEffect(() => {
+    dispatch(fetchTables());
+  }, [dispatch]);
 
   return (
     <div>
       <Container>
         <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/table/:id" element={<Table />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/table/:id" element={<CheckTable />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
         <Footer />
       </Container>
     </div>
   );
-}
+};
+
+const CheckTable = () => {
+  const { id } = useParams();
+  const tables = useSelector(state => state.tables);
+  const table = tables.find(table => table.id === id);
+  console.log(id)
+
+  if (!table) {
+    return <Navigate to="/" replace />;
+  }
+
+  return (
+    <Table />
+  );
+};
 
 export default App;
+
+
